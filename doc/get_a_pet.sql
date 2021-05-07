75a79e4b8901f8800e83bfbe3531e393d14e801c
Mira Hunt <mxh781@case.edu>
2021-05-07 18:16:07 -0400

    Update employee attributes
    Convert phoneNumber column to String

--- doc/get_a_pet.sql
+++ doc/get_a_pet.sql
@@ -394,7 +394,7 @@ CREATE TABLE "employee" (
   "lastName" char(20) DEFAULT NULL,
   "username" char(30) DEFAULT NULL,
   "password" char(30) DEFAULT NULL,
-  "phoneNumber" decimal(10,0) DEFAULT NULL,
+  "phoneNumber" char(10) DEFAULT NULL,
   PRIMARY KEY ("eid")
 );
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -405,7 +405,7 @@ CREATE TABLE "employee" (
 
 LOCK TABLES "employee" WRITE;
 /*!40000 ALTER TABLE "employee" DISABLE KEYS */;
-INSERT INTO "employee" VALUES (1,'Isaac','Morrison','imorrison@ccas.com','liklik123',2168897030),(2,'Lily','Rutherford','lrutherford@ccas.com','Pe@nut99',2166536654),(3,'Fiona','Reid','freid@ccas.com','8054',2167788054),(4,'Frank','Allan','fallan@ccas.com','qwerty',3174582256),(5,'Katherine','Rees','klr@parma.com','DontWorryBeHappy',2168835968),(6,'Tim','Hemmings','trh@parma.com','gg660',2162375506),(7,'Isaac','Vance','gfv@parma.com','hottie',6603451750),(8,'Gordon','Bell','gordbell@capl.com','passw0rd',2163682340),(9,'Sally','Vance','sallvanc@capl.com','wgrj253-o4gw',2162305968),(10,'Diana','Baker','dianbake@capl.com','crunch13srunch13',2162386038);
+INSERT INTO "employee" VALUES (1,'Isaac','Morrison','imorrison@ccas.com','liklik123','2168897030'),(2,'Lily','Rutherford','lrutherford@ccas.com','Pe@nut99','2166536654'),(3,'Fiona','Reid','freid@ccas.com','8054','2167788054'),(4,'Frank','Allan','fallan@ccas.com','qwerty','3174582256'),(5,'Katherine','Rees','klr@parma.com','DontWorryBeHappy','2168835968'),(6,'Tim','Hemmings','trh@parma.com','gg660','2162375506'),(7,'Isaac','Vance','gfv@parma.com','hottie','6603451750'),(8,'Gordon','Bell','gordbell@capl.com','passw0rd','2163682340'),(9,'Sally','Vance','sallvanc@capl.com','wgrj253-o4gw','2162305968'),(10,'Diana','Baker','dianbake@capl.com','crunch13srunch13','2162386038');
 /*!40000 ALTER TABLE "employee" ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -653,4 +653,4 @@ UNLOCK TABLES;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-07  3:01:44
+-- Dump completed on 2021-05-07 18:14:51
